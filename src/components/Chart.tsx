fcc5c279df887eb54a652aa89d3b805a45f37480
Mohammed Abu Mustafa <mea9294@rit.edu>
2023-10-13 17:39:24 +0400

    created Chart components and modified graphs page

---
+++ src/components/Chart.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+interface ChartProps {
+  data: any[]; // Pass your data as props
+}
+
+const Chart: React.FC<ChartProps> = ({ data }) => {
+  return (
+    <BarChart width={700} height={500} data={data}>
+      <XAxis dataKey="name" />
+      <YAxis />
+      <CartesianGrid stroke="#ccc" />
+      <Bar dataKey="value" fill="#8884d8" />
+      <Tooltip />
+      <Legend />
+    </BarChart>
+  );
+};
+
+export default Chart;
--- src/pages/graphs.tsx
+++ src/pages/graphs.tsx
@@ -1,13 +1,42 @@
-// src/pages/Page2.tsx
 import React from 'react';
+import Chart from '../components/Chart';
+import { Grid, Typography } from '@mui/material';
 
-const Graph: React.FC = () => {
+const chartData = [
+  { name: 'Jan', value: 100 },
+  { name: 'Feb', value: 200 },
+  { name: 'Mar', value: 150 },
+];
+
+const ChartPage: React.FC = () => {
   return (
     <div>
-      <h1>Page 2</h1>
-      <p>This is Page 2 content.</p>
+      <h1>Chart Page</h1>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Chart data={chartData} />
+          <Typography variant="subtitle1" align="center"width={700} height={50}>% of Dry Land: This graph shows regions of the land that lack adequate moisture, which might necessitate irrigation interventions or denote areas unsuitable for planting.
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Chart data={chartData} />
+          <Typography variant="subtitle1" align="center" width={700} height={50}>% of Unhealthy Plants :: An essential visual cue, this representation underscores areas where plants aren't thriving, potentially due to factors such as nutrient deficiencies or diseases.
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Chart data={chartData} />
+          <Typography variant="subtitle1" align="center" width={700} height={50}>% of Waterlogged Areas: An essential metric for certain crops, this graph showcases zones with an overabundance of moisture, which might be detrimental for plant roots and lead to issues like root rot.
+        </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Chart data={chartData} />
+          <Typography variant="subtitle1" align="center"  width={700} height={50}>
+            % of Pest-Infested Plants: Serving as an early warning system, this graph emphasizes areas under threat from pests, allowing farmers to take timely preventive or curative measures.
+          </Typography>
+        </Grid>
+      </Grid>
     </div>
   );
 };
 
-export default Graph;
+export default ChartPage;
